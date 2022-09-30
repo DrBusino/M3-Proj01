e91da53eee7792cccd4464eb87fc3242430099eb
DrBusino <pedrocasanova.cunha@gmail.com>
2022-09-29 21:42:23 -0300

    innitial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+
+
+const port = 3000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
+
